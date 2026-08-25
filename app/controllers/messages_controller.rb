d31d461033c6bf7291a3c0b16573bc5cd8c4eeb3
Tayayi Muyard <muyardmayam.t@gmail.com>
2026-08-25 17:40:25 +0200

    feat: implement conversations and messages controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def create
+    conversation = Conversation.find(params[:conversation_id])
+    unless conversation.participant?(current_user)
+      return redirect_to root_path, alert: "You don't have access to this chat."
+    end
+
+    message = conversation.messages.new(message_params)
+    message.user = current_user
+
+    if message.save
+      redirect_to conversation
+    else
+      @conversation = conversation
+      @messages = conversation.messages.order(:created_at)
+      @message = message
+      render "conversations/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
