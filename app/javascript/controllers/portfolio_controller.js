bfbf7fb9f99b5af6a91c041b0b64c5c0fe98ab6e
frederic ulrich <ulrich.frederic85@gmail.com>
2026-08-28 09:02:07 +0200

    add cover image selection from the portfolio

--- app/javascript/controllers/portfolio_controller.js
+++ app/javascript/controllers/portfolio_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "checkbox", "trash", "confirm" ]
+  static targets = [ "checkbox", "trash", "confirm", "pin", "cover" ]
 
   enable() {
     this.checkboxTargets.forEach((box) => box.classList.remove("d-none"))
     this.trashTarget.classList.add("d-none")
     this.confirmTarget.classList.remove("d-none")
+  }
+
+  pin() {
+    this.coverTargets.forEach((link) => link.classList.remove("d-none"))
+    this.pinTarget.classList.add("d-none")
   }
 }
