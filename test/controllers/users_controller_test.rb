53ccf67e9daaacecb6f3367261cd1124559d50c8
frederic ulrich <ulrich.frederic85@gmail.com>
2026-09-02 16:09:09 +0200

    Add names to liked artist tiles

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "show displays liked artists as image tiles with their name" do
+    user = User.create!(email: "likes@example.com", password: "password123", username: "likes-user", birthdate: 20.years.ago)
+    artist_user = User.create!(email: "artist@example.com", password: "password123", username: "liked-artist", birthdate: 25.years.ago)
+    artist_profile = artist_user.create_artist_profile!(display_name: "Ink Master")
+    artist_profile.portfolio_items.create!(image_url: "https://res.cloudinary.com/demo/image/upload/portfolio.jpg", position: 1)
+    user.likes.create!(artist_profile: artist_profile)
+
+    sign_in user
+    get user_path
+
+    assert_response :success
+    assert_select "a.liked-artist-tile[href='#{artist_path(artist_profile)}']", count: 1 do
+      assert_select "img.liked-artist-tile-image", count: 1
+      assert_select ".liked-artist-tile-name", text: "Ink Master", count: 1
+    end
+  end
 end
